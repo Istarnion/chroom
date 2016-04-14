380d2c19183c329d79785986b445797555620b6c
elodin <jorgen.skogstad@gmail.com>
2016-04-14 13:05:09 +0200

    made ws object on and connection in html, also simple forms

--- client/js/chatter.js
+++ client/js/chatter.js
@@ -1,5 +1,19 @@
 "use strict";
 
 $(function() {
-    console.log("DEBUG");
+
+  var ws = new WebSocket("ws://localhost:4242");
+
+  ws.onerror = (msg) => {
+      console.log(msg);
+      $("#connection_label").html("Not connected");
+    };
+
+  ws.onopen = () => {
+      $("#connection_label").html("Connected");
+    };
+
+  $("submit-button").on("click", () => {
+      console.log("submat");
+  });
 });
--- server/server.js
+++ server/server.js
@@ -5,11 +5,32 @@ var ws = require('ws');
 
 var http_server = express();
 
+var ws_server = new ws.Server({port:4242});
+
 http_server.use(express.static(__dirname + "/../client"));
 
+ws_server.on('connection', (connection) => {
+  console.log('Opened a connection');
+
+  connection.on('message', (message) => {
+    console.log("message: "+message);
+
+    for(var c=0;c<ws_server.clients.length;c++) {
+      ws_server.clients[c].send(message);
+    }
+  });
+
+  connection.on('close', () => {
+    console.log("Closed a connection");
+  });
+
+  connection.on('error', (error) => {
+    console.error("Error: "+error.message);
+  });
+});
 
 // Let node get a random port from the OS so we won't have issues with crashes
-var server = http_server.listen(() => {
+var server = http_server.listen(4241, () => {
     var host = server.address().address;
     if (host == '::') {
         host = 'localhost';
@@ -17,4 +38,3 @@ var server = http_server.listen(() => {
     var port = server.address().port;
     console.log('Running and listening at http://%s:%s', host, port);
 });
-
