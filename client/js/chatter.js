b8833971e2ec0d2bf46c2d9944021210f173e8e5
Istarnion <hallgeir@loekken.org>
2016-04-18 09:59:32 +0200

    Now storing nick in localstorage

--- client/js/chatter.js
+++ client/js/chatter.js
@@ -27,62 +27,75 @@ function getDateString() {
 }
 
 $(function() {
-  var chatwindow = $("#chatwindow");
-  var host = location.host;
-  var n = host.indexOf(":");
-  if (n > -1) {
-      host = host.substr(0, n);
-  }
-  console.log(host);
-  var ws = new WebSocket("ws://"+host+":4242");
-
-  ws.onerror = (msg) => {
-      console.log(msg);
-      $("#connection_label").html("Not connected");
-    };
-
-  ws.onopen = () => {
-      $("#connection_label").html("Connected");
-    };
-
-  ws.onmessage = (event) => {
-    var chatarea = $("#chatarea");
-
-    var json = JSON.parse(event.data);
-    var str =
-        '<span class="timestamp chat">'+"["+json.timestamp + "] </span>"+
-        '<span class="name chat">'+stringFormat("&lt"+  json.name + "&gt", 16, false)+'</span>'+
-        '<span class="message chat">'+json.msg+'</span>';
-
-    chatarea.html(chatarea.html() + "<br>"+str);
-
-    chatarea.scrollTop(chatarea[0].scrollHeight);
-
-  };
-
-  function sendMsg() {
     var namefield = $("#nameInput");
     var msgfield = $("#msgInput");
-    if(!namefield.val()) {
-       // Error
+    var chatwindow = $("#chatwindow");
+
+    var nick = window.localStorage.getItem("nick");
+    if (nick) {
+        namefield.val(nick);
     }
     else {
-      if(msgfield.val()) {
-        var datestring = getDateString();
-        var json = {name: namefield.val(), msg: msgfield.val(), timestamp: datestring};
-        ws.send(JSON.stringify(json));
-        $("#msgInput").val("");
+        namefield.val("Anon");
+    }
+
+    var host = location.host;
+    var n = host.indexOf(":");
+    if (n > -1) {
+        host = host.substr(0, n);
+    }
+    console.log(host);
+    var ws = new WebSocket("ws://"+host+":4242");
+
+    ws.onerror = (msg) => {
+        console.log(msg);
+        $("#connection_label").html("Not connected");
+    };
+
+    ws.onopen = () => {
+        $("#connection_label").html("Connected");
+      };
+
+    ws.onmessage = (event) => {
+      var chatarea = $("#chatarea");
+
+      var json = JSON.parse(event.data);
+      var str =
+          '<span class="timestamp chat">'+"["+json.timestamp + "] </span>"+
+          '<span class="name chat">'+stringFormat("&lt"+  json.name + "&gt", 16, false)+'</span>'+
+          '<span class="message chat">'+json.msg+'</span>';
+
+      chatarea.html(chatarea.html() + "<br>"+str);
+
+      chatarea.scrollTop(chatarea[0].scrollHeight);
+
+    };
+
+    function sendMsg() {
+
+      if(!namefield.val()) {
+         // Error
+      }
+      else {
+        window.localStorage.setItem("nick", namefield.val());
+
+        if(msgfield.val()) {
+          var datestring = getDateString();
+          var json = {name: namefield.val(), msg: msgfield.val(), timestamp: datestring};
+          ws.send(JSON.stringify(json));
+          $("#msgInput").val("");
+        }
       }
     }
-  }
 
-  $("#submit-button").click(() => {
-      sendMsg();
-  });
-
-  $("#msgInput").keyup((event) => {
-      if (event.keyCode === 13) {  // Enter
+    $("#submit-button").click(() => {
         sendMsg();
-      }
-  })
+    });
+
+    $("#msgInput").keyup((event) => {
+        if (event.keyCode === 13) {  // Enter
+          sendMsg();
+        }
+    })
 });
+
